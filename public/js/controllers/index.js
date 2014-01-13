5dda2c3ae9cd9f8d4f7c908630ef6e8455ba764c
Anthony Colas <anthony.colas@gmail.com>
2014-01-13 20:06:57 +0100

    add function to merge arrays and sum keys values

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -21,8 +21,6 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
             .sort(function (a, b) {
                 return a[0] - b[0];
             });
-
-        console.log("result : " + result);
         return result;
     };
 
@@ -33,14 +31,13 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
             $scope.games = games;
             $scope.myTotalScore = 0;
 
-            $scope.arrayDateAndScore = [];
-            $scope.arrayDateAndRank = [];
+            //$scope.arrayDateAndScore = [];
+            //$scope.arrayDateAndRank = [];
 
             $scope.usersArray = [];
 
             //on recupere les infos de chaque match
             jQuery.each(games, function (index) {
-
                 // on verifie si l user existe
                 var resultUserAlready = jQuery.grep($scope.usersArray, function (e) {
                     return e._id === games[index].user._id;
@@ -48,9 +45,8 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
                 if (resultUserAlready.length === 0) {
                     $scope.usersArray.push(games[index].user);
                     chart[games[index].user.email] = [];
-                    chart["rank_"+games[index].user.email] = [];
+                    chart["rank_" + games[index].user.email] = [];
                 }
-
                 // on verifie si l user existe avant de le mettre dans le tableau
                 var resultOpponentUserAlready = jQuery.grep($scope.usersArray, function (e) {
                     return e._id === games[index].opponent.user._id;
@@ -60,118 +56,70 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
                     chart[games[index].opponent.user.email] = [];
                     chart["rank_" + games[index].opponent.user.email] = [];
                 }
-
                 //on ajoute les points au bon utilisateur
-                var array = [];
-                array.push(moment.parseZone(this.date).format("DD-MMM-YYYY"));
-                array.push(games[index].details.points);
+                var arraySingleScore = [];
+                var arrayTotalScore = [];
+                var totalScore = 0;
+                var dateOfMatch = moment.parseZone(this.date).format("DD-MMM-YYYY");
+                arraySingleScore.push(dateOfMatch);
+                arrayTotalScore.push(dateOfMatch);
+                arraySingleScore.push(games[index].details.points);
+                var indexLength = 0;
                 if (games[index].myScore > games[index].opponent.score) {
-                    chart[games[index].user.email].push(array);
-                    chart["rank_" + games[index].user.email].push(array);
+                    chart[games[index].user.email].push(arraySingleScore);
+                    indexLength = chart["rank_" + games[index].user.email].length - 1;
+                    //find user and add score by date
+                    if (chart["rank_" + games[index].user.email][0] === undefined) {
+                        totalScore =  games[index].details.points;
+                    }
+                    else {
+                        totalScore = chart["rank_" + games[index].user.email][indexLength][1] + games[index].details.points;
+                    }
+                    arrayTotalScore.push(totalScore);
+                    chart["rank_" + games[index].user.email].push(arrayTotalScore);
                 } else {
-                    chart[games[index].opponent.user.email].push(array);
-                    chart["rank_" + games[index].opponent.user.email].push(array);
+                    chart[games[index].opponent.user.email].push(arraySingleScore);
+                    //find user and add score by date
+                    indexLength = chart["rank_" + games[index].opponent.user.email].length  - 1;
+                    if (chart["rank_" + games[index].opponent.user.email][0] === undefined) {
+                        totalScore =  games[index].details.points;
+                    }
+                    else {
+                        totalScore = chart["rank_" + games[index].opponent.user.email][indexLength][1] + games[index].details.points;
+                    }
+                    arrayTotalScore.push(totalScore);
+                    chart["rank_" + games[index].opponent.user.email].push(arrayTotalScore);
                 }
-
             });
 
+
+            //TODO rendre dynamique en fonction du nombre de users avec les contraintes jqplot
+
+            //calcul des points + somme des valeurs pour une meme date
             $scope.chartUser0Results = sum(chart[$scope.usersArray[0].email]);
             $scope.chartUser1Results = sum(chart[$scope.usersArray[1].email]);
             $scope.chartUser2Results = sum(chart[$scope.usersArray[2].email]);
 
-            $scope.chartUserORank = chart["rank_" + $scope.usersArray[0].email];
+            //calcul du classement
+            $scope.chartUserORanks = chart["rank_" + $scope.usersArray[0].email];
+            $scope.chartUser1Ranks = chart["rank_" + $scope.usersArray[1].email];
+            $scope.chartUser2Ranks = chart["rank_" + $scope.usersArray[2].email];
 
             //score of the others
             //console.dir($scope.usersArray);
-            /*
-             if (Global.user._id === this.user._id) {
-             $scope.arrayGame = [];
-             $scope.arrayRank = [];
-             $scope.arrayGame.push(moment.parseZone(this.date).format("DD-MMM-YYYY"));
-             $scope.arrayRank.push(moment.parseZone(this.date).format("DD-MMM-YYYY"));
-             $scope.myTotalScore += this.details.points;
-             $scope.arrayRank.push($scope.myTotalScore);
-             $scope.arrayGame.push(this.details.points);
-             $scope.arrayDateAndScore.push($scope.arrayGame);
-             $scope.arrayDateAndRank.push($scope.arrayRank);
-             }
-             /*
-             if (Global.user._id !== this.user._id) {
-             //ajout du score et des dates pour les autres joueurs
-             console.log("id opponent : " + this.opponent.user);
-             if(jQuery.inArray(this.opponent.user, $scope.usersArray) !== -1){
-             console.log("victoire ? " + this.details.victory);
-             console.log("le gagnant du match est : " + this.opponent.user);
-             console.log("le nombre de points gagnés est : " + this.details.points);
-             console.log("en date du : " + moment.parseZone(this.date).format("DD-MMM-YYYY"));
-             }
-             }
 
 
-             });
-             */
+            //on recupere les infos de chaque match
+            jQuery.each($scope.usersArray, function (index) {
+                if ($scope.usersArray[index]._id === Global.user._id) {
+                    $scope.myTotalScore = chart["rank_" + $scope.usersArray[index].email][chart["rank_" + $scope.usersArray[index].email].length - 1][1];
+                }
 
-            // merge array and sum same key values
-            /* var sums = {};
-             [$scope.arrayDateAndScore].forEach(function (array) {
-             array.forEach(function (pair) {
-             sums[pair[0]] = pair[1] + (sums[pair[0]] || 0);
-             });
-             });
-
-             var resultsArrayDateAndScore = [];
-             for (var key in sums) {
-             resultsArrayDateAndScore.push([key, sums[key]]);
-             }
-
-             // REFACTOR : merge array and sum same key values
-             var sums2 = {};
-             [$scope.arrayDateAndRank].forEach(function (array) {
-             array.forEach(function (pair) {
-             sums2[pair[0]] = pair[1];
-             });
-             });
-             var resultsArrayDateAndRank = [];
-             for (var key2 in sums2) {
-             resultsArrayDateAndRank.push([key2, sums2[key2]]);
-             }*/
-
-            //$scope.myScore = resultsArrayDateAndScore;
-            //$scope.myRank = resultsArrayDateAndRank;
-
-            $scope.Score1 = [
-                ["01-Jan-2014", 20],
-                ["04-Jan-2014", 10],
-                ["05-Jan-2014", 5],
-                ["06-Jan-2014", 50],
-                ["07-Jan-2014", 200]
-            ];
-            $scope.Rank1 = [
-                ["01-Jan-2014", 20],
-                ["04-Jan-2014", 30],
-                ["05-Jan-2014", 35],
-                ["06-Jan-2014", 85],
-                ["07-Jan-2014", 285]
-            ];
-
-            $scope.Score2 = [
-                ["01-Jan-2014", 5],
-                ["04-Jan-2014", 5],
-                ["05-Jan-2014", 5],
-                ["06-Jan-2014", 5],
-                ["07-Jan-2014", 5]
-            ];
-            $scope.Rank2 = [
-                ["01-Jan-2014", 5],
-                ["04-Jan-2014", 10],
-                ["05-Jan-2014", 15],
-                ["06-Jan-2014", 20],
-                ["07-Jan-2014", 25]
-            ];
+            });
 
 
-            $scope.chartOptionsDate = {
-                title: 'Default Date Axis',
+            $scope.chartOptionsDateAndScore = {
+                title: 'Nombre de points par match',
                 axes: {
                     xaxis: {
                         renderer: jQuery.jqplot.DateAxisRenderer
@@ -184,6 +132,9 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
                     yoffset: 12        // pixel offset of the legend box from the y (or y2) axis.
                 },
                 series: [
+                    {label: $scope.usersArray[2].firstName},
+                    {label: $scope.usersArray[1].firstName},
+                    {label: $scope.usersArray[0].firstName},
                     {
                         lineWidth: 4,
                         markerOptions: {
@@ -193,188 +144,219 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
                 ]
             };
 
-            $scope.chartOptions = {
-                // Turns on animatino for all series in this plot.
-                animate: true,
-                // Will animate plot on calls to plot1.replot({resetAxes:true})
-                animateReplot: true,
-                cursor: {
+            $scope.chartOptionsDateAndRank = {
+                title: 'Classement par date',
+                axes: {
+                    xaxis: {
+                        renderer: jQuery.jqplot.DateAxisRenderer
+                    }
+                },
+                legend: {
                     show: true,
-                    zoom: true,
-                    looseZoom: true,
-                    showTooltip: false
+                    location: 'ne',     // compass direction, nw, n, ne, e, se, s, sw, w.
+                    xoffset: 12,        // pixel offset of the legend box from the x (or x2) axis.
+                    yoffset: 12        // pixel offset of the legend box from the y (or y2) axis.
                 },
                 series: [
+                    {label: $scope.usersArray[2].firstName},
+                    {label: $scope.usersArray[1].firstName},
+                    {label: $scope.usersArray[0].firstName},
                     {
-                        pointLabels: {
-                            show: true
-                        },
-                        renderer: jQuery.jqplot.BarRenderer,
-                        showHighlight: false,
-                        yaxis: 'y2axis',
-                        rendererOptions: {
-                            // Speed up the animation a little bit.
-                            // This is a number of milliseconds.
-                            // Default for bar series is 3000.
-                            animation: {
-                                speed: 2500
-                            },
-                            barWidth: 15,
-                            barPadding: -15,
-                            barMargin: 0,
-                            highlightMouseOver: false
-                        }
-                    },
-                    {
-                        rendererOptions: {
-                            // speed up the animation a little bit.
-                            // This is a number of milliseconds.
-                            // Default for a line series is 2500.
-                            animation: {
-                                speed: 2000
-                            }
+                        lineWidth: 4,
+                        markerOptions: {
+                            style: 'square'
                         }
                     }
-                ],
-                axesDefaults: {
-                    pad: 0
-                },
-                axes: {
-                    // These options will set up the x axis like a category axis.
-                    xaxis: {
-                        renderer: jQuery.jqplot.DateAxisRenderer,
-                        tickOptions: {
-                            formatString: '%b&nbsp;%#d'
-                        }
-                    },
-                    yaxis: {
-                        tickOptions: {
-                            formatString: "%'d"
-                        },
-                        rendererOptions: {
-                            forceTickAt0: true
-                        }
-                    },
-                    y2axis: {
-                        tickOptions: {
-                            formatString: "%'d"
-                        },
-                        rendererOptions: {
-                            // align the ticks on the y2 axis with the y axis.
-                            alignTicks: true,
-                            forceTickAt0: true
-                        }
-                    }
-                },
-                highlighter: {
-                    show: true,
-                    showLabel: true,
-                    tooltipAxes: 'y',
-                    sizeAdjust: 7.5, tooltipLocation: 'ne'
-                }
+                ]
             };
+
         });
     };
 
-
-    //$scope.s1 = [['2008-09-30 4:00PM',4], ['2008-10-30 4:00PM',6.5], ['2008-11-30 4:00PM',5.7], ['2008-12-30 4:00PM',9], ['2009-01-30 4:00PM',8.2]];
-
-    //console.log($scope.s1);
-    //$scope.s2 = [['2013-12-12', 10200], ['2013-12-13', 10800]];
-
-    /*$scope.chartOptions = {
-     title:'Default Date Axis',
-     axes:{
-     xaxis:{
-     renderer:jQuery.jqplot.DateAxisRenderer
-     }
-     },
-     series:[{
-     lineWidth:4,
-     markerOptions:{
-     style:'square'
-     }
-     }]
-     };$/
-     /*$scope.chartOptions = {
-     // Turns on animatino for all series in this plot.
-     animate: true,
-     // Will animate plot on calls to plot1.replot({resetAxes:true})
-     animateReplot: true,
-     cursor: {
-     show: true,
-     zoom: true,
-     looseZoom: true,
-     showTooltip: false
-     },
-     series:[
-     {
-     pointLabels: {
-     show: true
-     },
-     renderer: jQuery.jqplot.BarRenderer,
-     showHighlight: false,
-     yaxis: 'y2axis',
-     rendererOptions: {
-     // Speed up the animation a little bit.
-     // This is a number of milliseconds.
-     // Default for bar series is 3000.
-     animation: {
-     speed: 2500
-     },
-     barWidth: 15,
-     barPadding: -15,
-     barMargin: 0,
-     highlightMouseOver: false
-     }
-     },
-     {
-     rendererOptions: {
-     // speed up the animation a little bit.
-     // This is a number of milliseconds.
-     // Default for a line series is 2500.
-     animation: {
-     speed: 2000
-     }
-     }
-     }
-     ],
-     axesDefaults: {
-     pad: 0
-     },
-     axes: {
-     // These options will set up the x axis like a category axis.
-     xaxis: {
-     renderer:jQuery.jqplot.DateAxisRenderer,
-     tickOptions:{
-     formatString:'%b&nbsp;%#d'
-     }
-     },
-     yaxis: {
-     tickOptions: {
-     formatString: "$%'d"
-     },
-     rendererOptions: {
-     forceTickAt0: true
-     }
-     },
-     y2axis: {
-     tickOptions: {
-     formatString: "$%'d"
-     },
-     rendererOptions: {
-     // align the ticks on the y2 axis with the y axis.
-     alignTicks: true,
-     forceTickAt0: true
-     }
-     }
-     },
-     highlighter: {
-     show: true,
-     showLabel: true,
-     tooltipAxes: 'y',
-     sizeAdjust: 7.5 , tooltipLocation : 'ne'
-     }
-     };*/
-
 }]);
+
+
+/*$scope.chartOptions = {
+ // Turns on animatino for all series in this plot.
+ animate: true,
+ // Will animate plot on calls to plot1.replot({resetAxes:true})
+ animateReplot: true,
+ cursor: {
+ show: true,
+ zoom: true,
+ looseZoom: true,
+ showTooltip: false
+ },
+ series: [
+ {
+ pointLabels: {
+ show: true
+ },
+ renderer: jQuery.jqplot.BarRenderer,
+ showHighlight: false,
+ yaxis: 'y2axis',
+ rendererOptions: {
+ // Speed up the animation a little bit.
+ // This is a number of milliseconds.
+ // Default for bar series is 3000.
+ animation: {
+ speed: 2500
+ },
+ barWidth: 15,
+ barPadding: -15,
+ barMargin: 0,
+ highlightMouseOver: false
+ }
+ },
+ {
+ rendererOptions: {
+ // speed up the animation a little bit.
+ // This is a number of milliseconds.
+ // Default for a line series is 2500.
+ animation: {
+ speed: 2000
+ }
+ }
+ }
+ ],
+ axesDefaults: {
+ pad: 0
+ },
+ axes: {
+ // These options will set up the x axis like a category axis.
+ xaxis: {
+ renderer: jQuery.jqplot.DateAxisRenderer,
+ tickOptions: {
+ formatString: '%b&nbsp;%#d'
+ }
+ },
+ yaxis: {
+ tickOptions: {
+ formatString: "%'d"
+ },
+ rendererOptions: {
+ forceTickAt0: true
+ }
+ },
+ y2axis: {
+ tickOptions: {
+ formatString: "%'d"
+ },
+ rendererOptions: {
+ // align the ticks on the y2 axis with the y axis.
+ alignTicks: true,
+ forceTickAt0: true
+ }
+ }
+ },
+ highlighter: {
+ show: true,
+ showLabel: true,
+ tooltipAxes: 'y',
+ sizeAdjust: 7.5, tooltipLocation: 'ne'
+ }
+ };*/
+
+
+
+
+
+//$scope.s1 = [['2008-09-30 4:00PM',4], ['2008-10-30 4:00PM',6.5], ['2008-11-30 4:00PM',5.7], ['2008-12-30 4:00PM',9], ['2009-01-30 4:00PM',8.2]];
+
+//console.log($scope.s1);
+//$scope.s2 = [['2013-12-12', 10200], ['2013-12-13', 10800]];
+
+/*$scope.chartOptions = {
+ title:'Default Date Axis',
+ axes:{
+ xaxis:{
+ renderer:jQuery.jqplot.DateAxisRenderer
+ }
+ },
+ series:[{
+ lineWidth:4,
+ markerOptions:{
+ style:'square'
+ }
+ }]
+ };$/
+ /*$scope.chartOptions = {
+ // Turns on animatino for all series in this plot.
+ animate: true,
+ // Will animate plot on calls to plot1.replot({resetAxes:true})
+ animateReplot: true,
+ cursor: {
+ show: true,
+ zoom: true,
+ looseZoom: true,
+ showTooltip: false
+ },
+ series:[
+ {
+ pointLabels: {
+ show: true
+ },
+ renderer: jQuery.jqplot.BarRenderer,
+ showHighlight: false,
+ yaxis: 'y2axis',
+ rendererOptions: {
+ // Speed up the animation a little bit.
+ // This is a number of milliseconds.
+ // Default for bar series is 3000.
+ animation: {
+ speed: 2500
+ },
+ barWidth: 15,
+ barPadding: -15,
+ barMargin: 0,
+ highlightMouseOver: false
+ }
+ },
+ {
+ rendererOptions: {
+ // speed up the animation a little bit.
+ // This is a number of milliseconds.
+ // Default for a line series is 2500.
+ animation: {
+ speed: 2000
+ }
+ }
+ }
+ ],
+ axesDefaults: {
+ pad: 0
+ },
+ axes: {
+ // These options will set up the x axis like a category axis.
+ xaxis: {
+ renderer:jQuery.jqplot.DateAxisRenderer,
+ tickOptions:{
+ formatString:'%b&nbsp;%#d'
+ }
+ },
+ yaxis: {
+ tickOptions: {
+ formatString: "$%'d"
+ },
+ rendererOptions: {
+ forceTickAt0: true
+ }
+ },
+ y2axis: {
+ tickOptions: {
+ formatString: "$%'d"
+ },
+ rendererOptions: {
+ // align the ticks on the y2 axis with the y axis.
+ alignTicks: true,
+ forceTickAt0: true
+ }
+ }
+ },
+ highlighter: {
+ show: true,
+ showLabel: true,
+ tooltipAxes: 'y',
+ sizeAdjust: 7.5 , tooltipLocation : 'ne'
+ }
+ };*/
